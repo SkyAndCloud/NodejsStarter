3c6f9a24e62393838a678e684865c5b980a5ad76
Ubuntu <ubuntu@ip-172-31-17-152.ap-northeast-1.compute.internal>
2017-12-14 07:28:30 +0000

    fix winston bugs

--- logger.js
+++ logger.js
@@ -1,13 +1,11 @@
-const winston=require('winston');
+const winston = require('winston');
 
-let ContactsLogger=new winston.Logger({
+exports.ContactsLogger = new winston.Logger({
     level: "verbose",
-    transports:[
+    transports: [
         new (winston.transports.File)({
-            filename:'logs/contacts.log'
+            filename: 'logs/contacts.log'
         }),
         new (winston.transports.Console)()
     ]
 })
-
-module.exports.logger=ContactsLogger;
